be31883ae0769ec6dd1e25b48f2bd87edfa0e42c
Aloyfarera <79070641+Aloyfarera@users.noreply.github.com>
2022-08-22 17:41:24 +0700

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ st.write("""
     ### Youtube Channel Analysis 
     """)
 
-youtube = build('youtube', 'v3', developerKey=api_key)
+youtube = build('youtube', 'v3', developerKey='AIzaSyAwHiDjI46hfBl-ieeMTIeDOL_Qdv2ePrg')
 
 def get_channel_stats(youtube, channel_id):
     request = youtube.channels().list(
